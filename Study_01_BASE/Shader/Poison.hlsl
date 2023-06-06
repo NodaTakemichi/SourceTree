42bd272f99cc22228b4111fe7712374eeebbd853
ASOST\2116224 <viored7007@gmail.com>
2023-06-06 16:37:28 +0900

    毒９割

--- Study_01_BASE/Shader/Poison.hlsl
+++ Study_01_BASE/Shader/Poison.hlsl
@@ -23,6 +23,60 @@ Texture2D g_SrcTexture:register(t0);
 //サンプラー：適切な色を決める
 SamplerState g_SrcSampler:register(s0);
 
+float4 PosCircle(float2 uv,float2 pos, float genTime)
+{
+	//発生時間
+	float gTime = g_time - genTime;
+
+
+	//進行度
+	//float circlePro = 1.0f - (cos(gTime * 3.0f) + 1.0f) / 2.0f;
+	float circlePro = 1.0f - (cos(gTime) + 1.0f) / 2.0f;
+	//初期半径
+	float initRadius = 0.0f;
+	//成長率
+	float grow = 0.12f;
+	//上昇速度
+	float upSpeed = 0.1f;
+
+	// 上昇
+	pos.y -= upSpeed * gTime;
+
+	//停止する指標
+	// ・半径
+	// ・時間
+	// ・割合？
+	//
+
+	//if (gTime >= 1.0f)
+	//{
+	//	return float4(0.0f, 0.0f, 0.0f, 0.0f);
+	//}
+
+
+	//座標差
+	float2 disPos = uv - pos;
+	//円の半径
+	float radius = initRadius + grow * saturate(circlePro);
+	//円の範囲内
+	float alpha = saturate(1.0f - length(disPos) / radius);
+	//円の最終的な色を決定
+	float4 circle = float4(0.64f, 0.218f, 1.0f, alpha * 0.8f);
+
+
+	//光線と座標位置のベクトル計算
+	float2 light = float2(0.0f, -1.0f);
+	//float2 shadowVec = normalize(disPos);
+	//内積
+	float d = dot(disPos, light);
+	//半径から距離が、遠
+	float len = saturate(radius * (0.7f + d));
+	circle.rg -= length(disPos) >= len ? float2(0.25f, 0.1f) : 0.0f;
+
+	return circle;
+
+}
+
 float4 main(PS_INPUT PSInput) : SV_TARGET
 {
 	//UV座標を受け取る
@@ -43,40 +97,35 @@ float4 main(PS_INPUT PSInput) : SV_TARGET
 
 
 	//玉模様
-	//進行度
-	float circlePro = 1.0f - (cos(g_time * 2.0f) + 1.0f) / 2.0f;
 	//座標
-	float2 pos = float2(0.2f, 0.9f);
-
-	//初期半径
-	float initRadius = 0.0f;
-	//成長率
-	float grow = 0.12f;
-	//上昇速度
-	float upSpeed = 0.05f;
-
-	// 上昇
-	float yOffset = upSpeed * g_time;
-	pos.y -= yOffset;
+	float2 pos = float2(0.8f, 0.8f);
+	//発生時間
+	float genTime = 0.0f;
 
 
-	//座標差
-	float2 disPos = uv - pos;
-	float radius = initRadius + grow * circlePro;
-	//円の最終的な色を決定
-	float alpha = saturate(1.0f - length(disPos) / radius);
-	float4 circle = float4(0.64f, 0.218f, 1.0f, alpha * 8.0f);
+	//毒玉
+	float4 circle = PosCircle(uv, pos, genTime);
+
+	//入れ物
+	float4 c = float4(0.0f, 0.0f, 0.0f, 0.0f);
 
 
-	//光線と座標位置のベクトル計算
-	float2 light = float2(0.0f, -1.0f);
-	//float2 shadowVec = normalize(disPos);
-	//内積
-	float d = dot(disPos, light);
-	//半径から距離が、遠
-	float len = radius * (0.7f + d);
-	circle.rg -= length(disPos) >= len ? float2(0.25f, 0.1f) : 0.0f;
+	//2
+	pos = float2(0.3f, 0.8f);
+	c = PosCircle(uv, pos, 0.4f);
+	circle = c .a > 0.0f ? c : circle;
 
+	//3
+	pos = float2(0.2f, 0.6f);
+	c = PosCircle(uv, pos, 0.9f);
+	circle = c.a > 0.0f ? c : circle;
+	//4
+	pos = float2(0.6f, 0.7f);
+	c = PosCircle(uv, pos, 1.2f);
+	circle = c.a > 0.0f ? c : circle;
+
+
+	//結果
 	disCol.rgb = circle.a > 0.0f ? circle.rgb : disCol.rgb;
 
 
